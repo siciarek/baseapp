467d6de0e768e41fd71e9b341b9422630d65c869
Jacek Siciarek <siciarek@wp.pl>
2015-11-23 21:32:49 +0100

    Apply bootstrap style to the KnpMenuBundle

--- src/Application/MainBundle/Menu/Builder.php
+++ src/Application/MainBundle/Menu/Builder.php
@@ -21,50 +21,55 @@ class Builder
      */
     public function __construct(FactoryInterface $factory, DataCollectorTranslator $translator, SecurityContext $securityContext)
     {
-        $this->factory = $factory;
-        $this->translator = $translator;
+        $this->factory         = $factory;
+        $this->translator      = $translator;
         $this->securityContext = $securityContext;
     }
 
     public function getMainMenu(array $options)
     {
         $menu = $this->factory->createItem('root');
+
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
-        
+
         $menu->addChild('Home', [ 'route' => 'default.home', 'label' => $this->translator->trans('Home') ]);
-        $menu->addChild('Info', [ 'route' => 'default.info', 'label' =>  $this->translator->trans('Info') ]);
-        $menu->addChild('About', [ 'route' => 'default.about', 'label' =>  $this->translator->trans('About') ]);
-        $menu->addChild('Contact', [ 'route' => 'default.contact', 'label' =>  $this->translator->trans('Contact') ]);
+        $menu->addChild('Info', [ 'route' => 'default.info', 'label' => $this->translator->trans('Info') ]);
+        $menu->addChild('About', [ 'route' => 'default.about', 'label' => $this->translator->trans('About') ]);
+        $menu->addChild('Contact', [ 'route' => 'default.contact', 'label' => $this->translator->trans('Contact') ]);
 
-        
-        if($this->securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            $menu->addChild('Private', [ 'route' => 'private.index', 'label' =>  $this->translator->trans('Private') ]);
 
-            if($this->securityContext->isGranted('ROLE_ADMIN')) {
-                $menu->addChild('Admin', [ 'route' => 'sonata_admin_dashboard', 'label' =>  $this->translator->trans('Admin') ]);
+        if ($this->securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $menu->addChild('Private', [ 'route' => 'private.index', 'label' => $this->translator->trans('Private') ]);
+
+            if ($this->securityContext->isGranted('ROLE_ADMIN')) {
+                $menu->addChild('Admin', [ 'route' => 'sonata_admin_dashboard', 'label' => $this->translator->trans('Admin') ]);
             }
-            
-            $menu->addChild('Sign out', [ 'route' => 'fos_user_security_logout', 'label' =>  $this->translator->trans('Sign out') ]);
-        }
-        else {
-            $menu->addChild('Sign in', [ 'route' => 'fos_user_security_login', 'label' =>  $this->translator->trans('Sign in') ]);
+
+            $menu->addChild('Sign out', [ 'route' => 'fos_user_security_logout', 'label' => $this->translator->trans('Sign out') ]);
+        } else {
+            $menu->addChild('Sign in', [ 'route' => 'fos_user_security_login', 'label' => $this->translator->trans('Sign in') ]);
         }
 
-        // TODO: redesign into dropdown menu
-        foreach(LocaleController::$locales as $locale => $name) {
-            $menu->addChild($name, [ 'route' => 'locale.switch', 'routeParameters' => [ 'locale' => $locale ]]);
-        }
+        $menu->addChild('Locale', [
+            'uri'                => '#',
+            'label'              => '<i class="fa fa-globe fa-lg fa-fw"></i> <span class="caret"></span>',
+            'extras'             => [ 'safe_label' => true ],
+            'attributes'         => [ ],
+            'linkAttributes'     => [
+                'class'         => 'dropdown dropdown-toggle',
+                'data-toggle'   => 'dropdown',
+                'role'          => 'button',
+                'aria-haspopup' => 'true',
+                'aria-expanded' => 'true',
+            ],
+            'childrenAttributes' => [
+                'class' => 'dropdown-menu',
+            ],
+        ]);
 
-        // $menu->addChild('Language', ['uri' => '#'])
-        //      ->setAttribute('class', 'dropdown')
-            //  ->setChildrenAttribute('class', 'dropdown-toggle')
-            //  ->setChildrenAttribute('data-toggle', 'dropdown')
-            //  ->setChildrenAttribute('role', 'button')
-            //  ->setChildrenAttribute('aria-haspopup', 'true')
-            //  ->setChildrenAttribute('aria-expanded', 'true')
-            //  ->setChildrenAttribute('class', 'dropdown-menu');
-        // $menu['Language']->addChild('Deutsch', array('uri' => '#'));
-        // $menu['Language']->addChild('English', array('uri' => '#'));
+        foreach (LocaleController::$locales as $locale => $name) {
+            $menu['Locale']->addChild($name, [ 'route' => 'locale.switch', 'routeParameters' => [ 'locale' => $locale ] ]);
+        }
 
         return $menu;
     }
