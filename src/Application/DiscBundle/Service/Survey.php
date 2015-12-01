8fb9a44399129d5511686a7d94a709360de2d5f5
Jacek Siciarek <siciarek@gmail.com>
2015-12-01 09:06:02 +0000

    Make tidy

--- src/Application/DiscBundle/Service/Survey.php
+++ src/Application/DiscBundle/Service/Survey.php
@@ -9,45 +9,13 @@ class Survey {
     
     public function getSurvey($id = 1)
     {
-        $survey = [
-            'mode'      => 'strict', // ['strict', 'soft']
-            'questions' => [ ],
-        ];
+        $fileName = realpath(__DIR__ . '/../Resources/config/surveys.yml');
+        $data = \Symfony\Component\Yaml\Yaml::parse(file_get_contents($fileName));
 
-        if($id === 2) {
-        
-            $url  = 'https://www.123test.com/disc-personality-test/';
-            $html = file_get_contents($url);
-    
-            $crawler = new Crawler($html);
-            $crawler = $crawler->filter('table.groep');
-    
-            foreach ($crawler as $element) {
-                // td class="term niets"
-                $q = [ ];
-                foreach ($element->getElementsByTagName('td') as $node) {
-                    $value = trim($node->nodeValue);
-                    if (!empty($value))
-                        $q[] = $value;
-                }
-    
-                $questions = array_combine([ 'D', 'i', 'S', 'C' ], $q);
-    
-                $survey['questions'][] = $questions;
-            }
-        }
-        else {
-            $fileName = realpath(__DIR__ . '/../Resources/config/surveys.yml');
-            $data = \Symfony\Component\Yaml\Yaml::parse(file_get_contents($fileName));
-            $elements = $data['surveys']['default'];
-            
-            foreach ($elements as $element => $q) {
-                
-                $questions = array_combine([ 'D', 'i', 'S', 'C' ], $q);
-    
-                $survey['questions'][] = $questions;
-            }
-        }
+        $temp = array_filter($data['Survey'], function($e) use ($id) {
+             return $e['id'] == $id;
+        });
+        $survey = array_pop($temp);
 
         return $survey;
     }
