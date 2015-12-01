b1d0d181ea74d0d6322362229b7007c5236e2673
Jacek Siciarek <siciarek@gmail.com>
2015-12-01 09:07:54 +0000

    Make tidy

--- src/Application/DiscBundle/Service/Survey.php
+++ src/Application/DiscBundle/Service/Survey.php
@@ -2,15 +2,15 @@
 namespace Application\DiscBundle\Service;
 
 use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\DomCrawler\Crawler;
-use Symfony\Component\CssSelector\CssSelector;
 
 class Survey {
     
     public function getSurvey($id = 1)
     {
+        // TODO: pomyśleć o bazie danych
+        
         $fileName = realpath(__DIR__ . '/../Resources/config/surveys.yml');
-        $data = \Symfony\Component\Yaml\Yaml::parse(file_get_contents($fileName));
+        $data = Yaml::parse(file_get_contents($fileName));
 
         $temp = array_filter($data['Survey'], function($e) use ($id) {
              return $e['id'] == $id;
