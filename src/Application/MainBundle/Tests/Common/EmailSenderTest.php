caa217182f1d4ba3b97b5eddf3d2c7ea52b37fd2
Jacek Siciarek <siciarek@gmail.com>
2015-12-22 15:30:39 +0100

    Fix email tests

--- src/Application/MainBundle/Tests/Common/EmailSenderTest.php
+++ src/Application/MainBundle/Tests/Common/EmailSenderTest.php
@@ -5,20 +5,20 @@ namespace Application\MainBundle\Tests\Common;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Application\MainBundle\Common\EmailSender;
 
-class EmailSenderTest extends WebTestCase
-{
+class EmailSenderTest extends WebTestCase {
+
     /**
      * @var \Application\MainBundle\Common\EmailSender
      */
     protected $srv;
     protected $container;
     protected $data = [];
-    
+
     /**
      * @group sender
      */
     public function testGeneral() {
-     
+
         $this->assertInstanceOf('Application\MainBundle\Common\EmailSender', $this->srv);
     }
 
@@ -26,28 +26,31 @@ class EmailSenderTest extends WebTestCase
      * @group sender
      */
     public function testSend() {
+        $command = 'php app/console --no-ansi swiftmailer:spool:send --env=test';
 
-        $this->data['before'] = trim(`app/console swiftmailer:spool:send --env=test`);        
-        $this->assertRegExp('/ 0 emails sent$/', $this->data['before']);
-        
+        `ant ccx`;
+
+        $result = trim(`$command`);
+
+        $this->assertRegExp('/ 0 emails sent$/', $result);
+
         $this->assertTrue($this->srv->send());
 
-        $this->data['after'] = trim(`app/console swiftmailer:spool:send --env=test`);
-        $this->assertRegExp('/ 1 emails sent$/', $this->data['after']);        
+        $result = trim(`$command`);
+
+        $this->assertRegExp('/ 1 emails sent$/', $result);
     }
 
     public function tearDown() {
 
     }
-    
+
     public function setUp() {
 
-        echo `ant ccx`;
-        echo `app/console cache:warmup --env=test`;
-        
         self::bootKernel();
         $this->container = self::$kernel->getContainer();
         $this->srv = $this->container->get('app.common.email.sender');
         $this->data = [];
-    }   
+    }
+
 }
