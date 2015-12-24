8a5375830b7fd79e34668fafa642a942b7a210cc
Jacek Siciarek <siciarek@gmail.com>
2015-12-24 15:19:57 +0100

    Fix wysyłania emaili

--- src/Application/MainBundle/Controller/CommonController.php
+++ src/Application/MainBundle/Controller/CommonController.php
@@ -11,28 +11,55 @@ use Symfony\Component\HttpFoundation\Request;
 abstract class CommonController extends Controller {
 
     /**
+     * Zwraca dane JSON wysłane postem jako tablicę lub obiekt
+     *
+     * @param boolean $array czy zwracać jako tablicę
+     * @return array|mixed
+     */
+    protected function getJsonRequest(Request $request, $array = true) {
+        $input = $request->get('json');
+
+        if ($input === null) {
+            $input = file_get_contents('php://input');
+        }
+
+        $data = json_decode($input, $array);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $data = $this->get('app.common.laaf.frame')->getErrorFrame();
+            $data['msg'] = json_last_error_msg();
+            $data['data'] = [];
+            $data['data']['code'] = 500;
+            $data['data']['input'] = $input;
+
+            $data = json_decode(json_encode($data), $array);
+        }
+
+        return $data;
+    }
+
+    /**
      * Handles json response
-     * 
+     *
      * @param type $func callable
      */
     protected function handleJsonAction($func) {
         try {
             $frame = $func();
-        }  catch (\Exception $e) {
-            
+        } catch (\Exception $e) {
+
             $frame = $this->get('app.common.laaf.frame')
                     ->getErrorFrame('Unexpected Exception.');
-            
+
             if ($this->get('kernel')->getEnvironment() != 'prod') {
-               $frame['msg'] = $e->getMessage();
-               $frame['data'] = $e->getTrace();
+                $frame['msg'] = $e->getMessage();
+                $frame['data'] = $e->getTrace();
             }
         }
-        
+
         return $this->getJsonResponse($frame);
     }
-    
-    
+
     /**
      * Returns json response.
      */
