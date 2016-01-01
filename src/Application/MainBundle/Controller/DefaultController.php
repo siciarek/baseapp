dd2217b847f901d81e549e01678ac0172e076bc2
Jacek Siciarek <siciarek@gmail.com>
2016-01-01 16:34:45 +0100

    git diff

--- src/Application/MainBundle/Controller/DefaultController.php
+++ src/Application/MainBundle/Controller/DefaultController.php
@@ -10,10 +10,49 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends CommonController
 {
+
+    /**
+     * @Route("/file-pdf", name="default.pdf.test")
+     */
+    public function pdfAction()
+    {
+
+        $html = <<<HTML
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />      
+</head>
+<body>
+                
+<h1>Zażółć gęślą jaźń</h1>    
+<hr/>    
+<p>Agent 007 powrócił jak zwykle w towarzystwie pięknych kobiet. Na londyńskiej premierze "Spectre" odtwórca głównej roli Daniel Craig pojawił się w towarzystwie obu partnerek z planu: Moniki Bellucci i Lei Seydoux. - W tym filmie jest może nieco więcej ekstrawagancji i takiego staroświeckiego splendoru - przekonywał reżyser Sam Mendes. A Craig podkreślał, że "każdy dzień na planie był wspaniały". Do kin w Polsce film trafi 6 listopada. (http://www.tvn24.pl)</p>
+
+<div>
+<img src="http://i.kinja-img.com/gawker-media/image/upload/wpkp4may9t8nsljlztfh.jpg"/>
+</div>
+</body>    
+</html>
+HTML;
+        $srv = $this->get('knp_snappy.pdf');
+
+//        $html = $this->renderView('MyBundle:Foo:bar.html.twig', array(
+//            'some' => $vars
+//        ));
+        
+        $headers = [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'filename="document.pdf"'
+        ];
+
+        return new Response($srv->getOutputFromHtml($html), 200, $headers);
+    }
+
     /**
      * @Route("/json-test", name="default.json.test")
      */
-    public function jsonAction() {
+    public function jsonAction()
+    {
         $data = [];
 
         $auth = [
@@ -25,7 +64,7 @@ class DefaultController extends CommonController
 
         return $this->getJsonResponse($frame);
     }
-    
+
     /**
      * @Route("/about", name="default.about")
      * @Template()
@@ -34,7 +73,7 @@ class DefaultController extends CommonController
     {
         return [];
     }
-    
+
     /**
      * @Route("/contact", name="default.contact")
      * @Template()
@@ -42,8 +81,8 @@ class DefaultController extends CommonController
     public function contactAction(Request $request)
     {
         return [];
-    }    
-    
+    }
+
     /**
      * @Route("/", name="default.home")
      * @Template()
@@ -61,4 +100,5 @@ class DefaultController extends CommonController
     {
         return [];
     }
+
 }
