549f4b42d8d01ec6863c6ab2c755c6b2159e0894
Jacek Siciarek <siciarek@gmail.com>
2016-01-07 10:47:45 +0100

    Modify sample command - add process lock

--- src/Application/MainBundle/Command/GoCommand.php
+++ src/Application/MainBundle/Command/GoCommand.php
@@ -7,12 +7,15 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\LockHandler;
 
-class GoCommand extends ContainerAwareCommand {
+class GoCommand extends ContainerAwareCommand
+{
 
-    protected function configure() {
+    protected function configure()
+    {        
         $this
-                ->setName('go:show')
+                ->setName('go')
                 ->setDescription('Symfony 2 command template.')
                 ->addOption(
                         'yell', 'y', InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters'
@@ -23,10 +26,21 @@ class GoCommand extends ContainerAwareCommand {
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Create the lock:
+        $lock = new LockHandler('go');
+        if (!$lock->lock()) {            
+            return 0xFF;
+        }
+        
         $messages = __METHOD__;
         $output->writeln($messages);
+        
+        sleep(10);
+        
+        // Release the lock:
+        $lock->release();
     }
 
 }
