6333afeba8caa9669a3d59130ae9f20a46c01be6
Jacek Siciarek <siciarek@gmail.com>
2016-01-07 21:10:19 +0100

    Registration form update - added captcha

---
+++ src/Application/MainBundle/Common/Form/RegistrationType.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Application\MainBundle\Common\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('captcha', 'captcha', [
+            'label' => ' ',
+            'attr' => [
+                'placeholder' => 'captcha',
+            ],
+        ]);
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'app_user_registration';
+    }
+
+}
