8e9e811823cb929bb449fdfa93905440081e270c
Jacek Siciarek <siciarek@gmail.com>
2016-01-14 16:11:58 +0100

    Database dumper update

---
+++ src/Application/MainBundle/Command/DumpDbCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Application\MainBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\LockHandler;
+
+class DumpDbCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+                ->setName('dumpdb')
+                ->setDescription('Sync your local database with the remote one.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dir = implode(DIRECTORY_SEPARATOR, [
+            $this->getContainer()->get('kernel')->getRootDir(),
+            '..',
+            'utils',
+            'dumpdb',
+        ]);
+        
+        $command = implode(DIRECTORY_SEPARATOR, [
+            $dir,
+            'dumpdb',
+        ]);
+
+        chdir($dir);
+        
+        $process = new \Symfony\Component\Process\Process($command);
+        $process->mustRun();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $stderr = $process->getErrorOutput();
+        $stderr = trim($stderr);
+
+        $stdout = $process->getOutput();
+
+        $result = implode("\n\n", [$stderr, $stdout]);
+
+
+        $output->writeln($result);
+    }
+
+}
