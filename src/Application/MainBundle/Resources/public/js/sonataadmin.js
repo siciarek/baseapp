b10672cc58724b231fc8cf92ad41daa13a62689a
Jacek Siciarek <siciarek@gmail.com>
2016-01-15 01:01:05 +0100

    Update page title when is not set yet

--- src/Application/MainBundle/Resources/public/js/sonataadmin.js
+++ src/Application/MainBundle/Resources/public/js/sonataadmin.js
@@ -1,33 +1,50 @@
-// Add locale switcher
+var locales = {
+    pl: 'Polski',
+    en: 'English'
+};
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var locales = {
-        pl: 'Polski',
-        en: 'English'
-    };
+// When the page has not title set, the page name is set as a title:
+
+    $('form[action^="/admin/application/main/page/create?"]')
+            .on('keyup', 'input[id$=_name]', function (e) {
+                var self = $(this);
+                var form = self.closest('form');
+                var tit = form.find('input[id$=_title]');
+
+                var name = self.val();
+                var title = $(tit.get(0)).val();
+
+                if (name.slice(0, title.length) === title || title.slice(0, name.length) === name) {
+                    tit.val(name);
+                }
+            })
+            ;
+
+// Add locale switcher:
 
     var positions = '';
 
-    for(var locale in locales) {
+    for (var locale in locales) {
 
-        var url = Routing.generate('locale.switch', { locale: locale });
+        var url = Routing.generate('locale.switch', {locale: locale});
 
         positions += '<li role="presentation">'
-            + '<a role="menuitem" tabindex="-1" href="' + url + '">' + locales[locale] + '</a>'
-            + '</li>';
+                + '<a role="menuitem" tabindex="-1" href="' + url + '">' + locales[locale] + '</a>'
+                + '</li>';
     }
-       
+
     var menu = ''
-        + '<li class="dropdown locale-switch">'
-        + '    <a href="#" data-toggle="dropdown" class="dropdown-toggle">'
-        + '        <i class="fa fa-globe fa-    lg fa-fw"></i> <i class="fa fa-caret-down"></i>'
-        + '    </a>'
-        + '    <ul class="dropdown-menu">'
-        + positions
-        + '    </ul>'
-        + '</li>'
-        ;
+            + '<li class="dropdown locale-switch">'
+            + '    <a href="#" data-toggle="dropdown" class="dropdown-toggle">'
+            + '        <i class="fa fa-globe fa-    lg fa-fw"></i> <i class="fa fa-caret-down"></i>'
+            + '    </a>'
+            + '    <ul class="dropdown-menu">'
+            + positions
+            + '    </ul>'
+            + '</li>'
+            ;
 
     $('.navbar.navbar-static-top .navbar-custom-menu ul.nav.navbar-nav').prepend(menu);
 
